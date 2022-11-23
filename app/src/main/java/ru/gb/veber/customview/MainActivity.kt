30a33705e256e5f83c984b0ef1ee12c4994eb413
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-11-23 23:49:20 +0300

    Ах

--- app/src/main/java/ru/gb/veber/customview/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/customview/MainActivity.kt
@@ -1,42 +1,34 @@
 package ru.gb.veber.customview
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
-import androidx.annotation.RequiresApi
+import kotlinx.coroutines.*
 import ru.gb.veber.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val token = Any()
-    private val handler = Handler(Looper.getMainLooper())
+    private val scope = CoroutineScope(Dispatchers.Main)
 
-    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
         binding.bottomButtons.setListener {
             if (it == BottomButtonAction.POSITIVE) {
-                binding.bottomButtons.isProgressMode = true
-                handler.postDelayed({
+                scope.launch {
+                    binding.bottomButtons.isProgressMode = true
+                    delay(2000)
                     binding.bottomButtons.isProgressMode = false
                     binding.bottomButtons.setPositiveButtonText("Updated OK")
-                    Toast.makeText(this, "Positive Button Pressed", Toast.LENGTH_SHORT).show()
-                }, token, 2000)
-
+                }
             } else if (it == BottomButtonAction.NEGATIVE) {
                 binding.bottomButtons.setNegativeButtonText("Updated CANCEL")
-                Toast.makeText(this, "NEGATIVE Button Pressed", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacksAndMessages(token)
+        scope.cancel()
     }
 }
