b702a210570ad69372eaf5eb08ea990283673e2a
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-11-24 14:56:17 +0300

    initial commit

--- app/src/main/java/ru/gb/veber/customview/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/customview/MainActivity.kt
@@ -7,17 +7,8 @@ import ru.gb.veber.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
-        binding.bottomButtons.setListener {
-            if (it == BottomButtonAction.POSITIVE) {
-                binding.bottomButtons.setPositiveButtonText("Updated OK")
-            } else if (it == BottomButtonAction.NEGATIVE) {
-                binding.bottomButtons.setNegativeButtonText("Updated CANCEL")
-            }
-        }
     }
 }
