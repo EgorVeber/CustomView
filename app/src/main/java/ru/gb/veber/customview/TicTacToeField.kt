fc8172f93571e3ae09f5403705418b41e84c8bb1
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-11-24 19:57:13 +0300

    Copy example

--- app/src/main/java/ru/gb/veber/customview/TicTacToeField.kt
+++ app/src/main/java/ru/gb/veber/customview/TicTacToeField.kt
@@ -1,18 +1,32 @@
 package ru.gb.veber.customview
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 enum class Cell {
-    PLAYER_1,
-    PLAYER_2,
-    EMPTY
+    PLAYER_1, // X
+    PLAYER_2, // O
+    EMPTY     // empty cell
 }
 
-typealias OnFieldChangedListener = (field: TicTacToeField) -> (Unit)
+typealias OnFieldChangedListener = (field: TicTacToeField) -> Unit
 
-class TicTacToeField(val rows: Int, val columns: Int) {
-
-    private val cells = Array(rows) { Array(columns) { Cell.EMPTY } }
+/**
+ * Data class for [TicTacToeView]
+ */
+class TicTacToeField private constructor(
+    val rows: Int,
+    val columns: Int,
+    private val cells: Array<Array<Cell>>
+) {
 
     val listeners = mutableSetOf<OnFieldChangedListener>()
+
+    constructor(rows: Int, columns: Int) : this(
+        rows,
+        columns,
+        Array(rows) { Array(columns) { Cell.EMPTY } }
+    )
 
     fun getCell(row: Int, column: Int): Cell {
         if (row < 0 || column < 0 || row >= rows || column >= columns) return Cell.EMPTY
@@ -23,9 +37,26 @@ class TicTacToeField(val rows: Int, val columns: Int) {
         if (row < 0 || column < 0 || row >= rows || column >= columns) return
         if (cells[row][column] != cell) {
             cells[row][column] = cell
-            listeners.forEach {
-                it?.invoke(this)
-            }
+            listeners.forEach { it?.invoke(this) }
         }
     }
+
+    fun saveState(): Memento {
+        // ideally cells should be copied (e.g. Arrays.copyOf)
+        return Memento(rows, columns, cells)
+    }
+
+    @Suppress("ArrayInDataClass")
+    @Parcelize
+    data class Memento(
+        private val rows: Int,
+        private val columns: Int,
+        private val cells: Array<Array<Cell>>
+    ) : Parcelable {
+        fun restoreField(): TicTacToeField {
+            // ideally cells should be copied (e.g. Arrays.copyOf)
+            return TicTacToeField(rows, columns, cells)
+        }
+    }
+
 }
